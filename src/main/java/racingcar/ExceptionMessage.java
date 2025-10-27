3e8d48102c77c07d506c3fbceae91e1fb7c553d4
tichall <0817frog@gmail.com>
2025-10-27 22:04:08 +0900

    feat(Race): Race class 구현
    실제 경주 과정을 진행합니다.
    - 경주 시작 전 자동차, 시도 횟수 설정
    - 자동차 전진 시도
    - 경주 결과 출력

--- src/main/java/racingcar/ExceptionMessage.java
+++ src/main/java/racingcar/ExceptionMessage.java
@@ -2,6 +2,7 @@ package racingcar;
 
 public enum ExceptionMessage {
     PARSE_INT_ERROR("유효한 정수값을 입력해주세요."),
+    ATTEMPT_COUNT_MUST_BE_POSITIVE("시도 횟수는 양수여야 합니다."),
     DELIMITER_CANNOT_END_WITH("구분자로 끝나면 안됩니다."),
     NAME_CANNOT_BE_EMPTY("이름은 비어있으면 안됩니다."),
     NAME_LENGTH_EXCEEDED("이름 길이는 5자를 초과할 수 없습니다.")
